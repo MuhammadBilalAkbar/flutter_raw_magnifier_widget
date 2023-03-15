8cc6516ae9b370c175497fba5121851c9d331b25
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-03-15 10:25:02 +0500

    Review1

--- README.md
+++ README.md
@@ -52,7 +52,43 @@
 
 1. In `main.dart`, theme properties are set.
 2. In `my_homepage.dart`, initialize Offset named dragGesturePosition. Offset is an immutable 2D floating-point offset.
-<br/>
 ```dart
   var dragGesturePosition = Offset.zero;
+```
+Offsets can be explained in two ways:
+- As representing a point in Cartesian space a specified distance from a separately-maintained origin.
+- As a vector that can be applied to coordinates.
+3. `scaffold` contains `column` and `column` contains `stack`. `stack` contains two children:
+- `GestureDetector` with child as `column` containing `Text` and `FlutterLogo`.
+```dart
+                    GestureDetector(
+                      onPanUpdate: (details) => setState(() {
+                        dragGesturePosition = details.localPosition;
+                      }),
+                      child: Column(
+                        children: const [
+                          Text('Drag on the logo!'),
+                          SizedBox(height: 20),
+                          FlutterLogo(size: 200),
+                        ],
+                      ),
+                    ),
+```
+- `Positioned` widget containing `RawMagnifier`.
+```dart
+                    Positioned(
+                      left: dragGesturePosition.dx,
+                      top: dragGesturePosition.dy,
+                      child: const RawMagnifier(
+                        size: Size(200, 200),
+                        decoration: MagnifierDecoration(
+                          opacity: 1,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.pink, width: 3),
+                          ),
+                        ),
+                        focalPointOffset: Offset.zero,
+                        magnificationScale: 2,
+                      ),
+                    ),
 ```
--- lib/my_homepage.dart
+++ lib/my_homepage.dart
@@ -20,38 +20,36 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                RepaintBoundary(
-                  child: Stack(
-                    children: [
-                      GestureDetector(
-                        onPanUpdate: (details) => setState(() {
-                          dragGesturePosition = details.localPosition;
-                        }),
-                        child: Column(
-                          children: const [
-                            Text('Drag on the logo!'),
-                            SizedBox(height: 20),
-                            FlutterLogo(size: 200),
-                          ],
-                        ),
+                Stack(
+                  children: [
+                    GestureDetector(
+                      onPanUpdate: (details) => setState(() {
+                        dragGesturePosition = details.localPosition;
+                      }),
+                      child: Column(
+                        children: const [
+                          Text('Drag on the logo!'),
+                          SizedBox(height: 20),
+                          FlutterLogo(size: 200),
+                        ],
                       ),
-                      Positioned(
-                        left: dragGesturePosition.dx,
-                        top: dragGesturePosition.dy,
-                        child: const RawMagnifier(
-                          size: Size(200, 200),
-                          decoration: MagnifierDecoration(
-                            opacity: 1,
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(color: Colors.pink, width: 3),
-                            ),
+                    ),
+                    Positioned(
+                      left: dragGesturePosition.dx,
+                      top: dragGesturePosition.dy,
+                      child: const RawMagnifier(
+                        size: Size(200, 200),
+                        decoration: MagnifierDecoration(
+                          opacity: 1,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.pink, width: 3),
                           ),
-                          focalPointOffset: Offset.zero,
-                          magnificationScale: 2,
                         ),
+                        focalPointOffset: Offset.zero,
+                        magnificationScale: 2,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ],
             ),
